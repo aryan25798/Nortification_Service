2de64a08af148d8842712326269c63ee5cac883d
Aryan <120791947+aryan25798@users.noreply.github.com>
2025-05-18 02:03:53 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -3,17 +3,16 @@ from datetime import datetime
 
 class NotificationCreate(BaseModel):
     user_id: int
-    type: str  # "email", "sms", or "in-app"
+    type: str
     message: str
 
 class NotificationResponse(BaseModel):
-    id: int
+    id: str
     user_id: int
     type: str
     message: str
     status: str
     created_at: datetime
 
-    model_config = {
-        "from_attributes": True
-    }
+    class Config:
+        orm_mode = True
